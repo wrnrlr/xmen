993b22472b89c895af4888a9a9663751b937c81e
wrnrlr <werner@alman.ax>
2025-07-27 19:17:27 +0200

    Fix dom tests

--- src/dom.zig
+++ src/dom.zig
@@ -28,7 +28,22 @@ pub const Node = union(NodeType) {
       }
     }
 
-    pub fn setParentElement(self: Node, parent:*Node) void {
+    pub fn appendChild(self: Node, child: *Node) !*Node {
+      return switch (self) {
+          .document => |doc| doc.appendChild(@constCast(&self), child),
+          .element => |elem| elem.appendChild(@constCast(&self), child),
+          else => @panic("oops")
+      };
+    }
+
+    pub fn setAttributeNode(node: Node, attr: *Node) void {
+      switch (node) {
+          .element => |elem| elem.setAttributeNode(node, attr),
+          else => {}
+      }
+    }
+
+    pub fn setParentElement(self: Node, parent: *Node) void {
         switch (self) {
             .document => null,
             .element => |elem| elem.parentElement = parent,
@@ -222,6 +237,10 @@ pub const Attr = struct {
     value: []const u8,
     parentElement: ?*Node = null,
 
+    pub fn init(name: []const u8, value: []const u8, parentElement: ?*Node) Attr {
+      return Attr{ .name = name, .value = value, .parentElement = parentElement};
+    }
+
     fn render(self: Attr, writer: anytype) RenderError!void {
         try writer.print(" {s}=\"{s}\"", .{ self.name, self.value });
     }
@@ -230,48 +249,40 @@ pub const Attr = struct {
 const testing = std.testing;
 
 test "Node.getNodeType" {
-    var doc = Document.init(testing.allocator);
-    var elem = Element.init(testing.allocator, "div");
-    var text = Text{ .content = "Hello" };
-    var attr = Attr{ .name = "id", .value = "test" };
+    var doc = Node{ .document = @constCast(&Document.init(testing.allocator)) };
+    try testing.expectEqual(NodeType.document, doc.getNodeType());
 
-    var doc_node = Node{ .document = &doc };
-    var elem_node = Node{ .element = &elem };
-    var text_node = Node{ .text = &text };
-    var attr_node = Node{ .attribute = &attr };
+    var elem = Node{ .element = @constCast(&Element.init(testing.allocator, "div")) };
+    try testing.expectEqual(NodeType.element, elem.getNodeType());
 
-    try testing.expectEqual(NodeType.document, doc_node.getNodeType());
-    try testing.expectEqual(NodeType.element, elem_node.getNodeType());
-    try testing.expectEqual(NodeType.text, text_node.getNodeType());
-    try testing.expectEqual(NodeType.attribute, attr_node.getNodeType());
+    var text = Node{ .text = @constCast(&Text{ .content = "Hello" }) };
+    try testing.expectEqual(NodeType.text, text.getNodeType());
+
+    var attr = Node{ .attribute = @constCast(&Attr{ .name = "id", .value = "test" }) };
+    try testing.expectEqual(NodeType.attribute, attr.getNodeType());
 }
 
 test "Node.parentElement" {
-    var doc = Document.init(testing.allocator);
-    var elem = Element.init(testing.allocator, "div");
-    var text = Text{ .content = "Hello" };
-    var attr = Attr{ .name = "id", .value = "test" };
+    var doc = Node{ .document = @constCast(&Document.init(testing.allocator)) };
+    try testing.expectEqual(null, doc.getParentElement());
 
-    var doc_node = Node{ .document = &doc };
-    try testing.expectEqual(null, doc_node.getParentElement());
+    var elem = Node{ .element = @constCast(&Element.init(testing.allocator, "div")) };
+    try testing.expectEqual(null, elem.getParentElement());
 
-    var elem_node = Node{ .element = &elem };
-    try testing.expectEqual(null, elem_node.getParentElement());
+    elem.element.parentElement = &doc;
+    try testing.expectEqual(&doc, elem.getParentElement());
 
-    elem_node.element.parentElement = &doc_node;
-    try testing.expectEqual(&doc_node, elem_node.getParentElement());
+    var text = Node{ .text = @constCast(&Text{ .content = "Hello" }) };
+    try testing.expectEqual(null, text.getParentElement());
 
-    var text_node = Node{ .text = &text };
-    try testing.expectEqual(null, text_node.getParentElement());
+    text.text.parentElement = &elem;
+    try testing.expectEqual(&elem, text.getParentElement());
 
-    text_node.text.parentElement = &elem_node;
-    try testing.expectEqual(&elem_node, text_node.getParentElement());
+    var attr = Node{ .attribute = @constCast(&Attr{ .name = "id", .value = "test" }) };
+    try testing.expectEqual(null, attr.getParentElement());
 
-    var attr_node = Node{ .attribute = &attr };
-    try testing.expectEqual(null, attr_node.getParentElement());
-
-    attr_node.attribute.parentElement = &elem_node;
-    try testing.expectEqual(&elem_node, attr_node.getParentElement());
+    attr.attribute.parentElement = &elem;
+    try testing.expectEqual(&elem, attr.getParentElement());
 }
 
 test "Element.init" {
@@ -284,35 +295,37 @@ test "Element.init" {
 }
 
 test "Element.{set|get}Attribute" {
-    var elem = Element.init(testing.allocator, "div");
-    // This line should now work with the updated signature; no change needed, but confirming for context
-    var elemNode = Node{ .element = &elem };
-    const newAttrId = try testing.allocator.create(Attr);
-    newAttrId.* = Attr{ .name = "id", .value = "test", .parentElement = &elemNode };
-    const attrNode = Node{ .attribute = newAttrId };
-    try elem.setAttributeNode(&elemNode, @constCast(&attrNode));
-    const newAttrClass = try testing.allocator.create(Attr);
-    newAttrClass.* = Attr{ .name = "class", .value = "container", .parentElement = &elemNode };
-    const attrNode2 = Node{ .attribute = newAttrClass };
-    try elem.setAttributeNode(&elemNode, @constCast(&attrNode2));
+    var elem = Node{ .element = @constCast(&Element.init(testing.allocator, "a")) };
+    const id = Node{ .attribute = @constCast(&Attr.init("id", "1", &elem)) };
+    try elem.element.setAttributeNode(&elem, @constCast(&id));
+    const class = Node{ .attribute = @constCast(&Attr.init("class", "a", &elem )) };
+    try elem.element.setAttributeNode(&elem, @constCast(&class));
 
-    try testing.expectEqualStrings("test", elem.getAttributeNode("id").?.attribute.value);
-    try testing.expectEqualStrings("container", elem.getAttributeNode("class").?.attribute.value);
-    try testing.expectEqual(@as(?*Node, null), elem.getAttributeNode("style"));
-    const newAttrNewId = try testing.allocator.create(Attr);
-    newAttrNewId.* = Attr{ .name = "id", .value = "new-id", .parentElement = &elemNode };
-    var attrNode3 = Node{ .attribute = newAttrNewId };
-    try elem.setAttributeNode(&elemNode, &attrNode3);
-    try testing.expectEqualStrings("new-id", elem.getAttributeNode("id").?.attribute.value);
-    try testing.expectEqual(@as(usize, 2), elem.attributes.items.len);
-    testing.allocator.destroy(newAttrId);
-    testing.allocator.destroy(newAttrClass);
-    testing.allocator.destroy(newAttrNewId);
+    try testing.expectEqualStrings("1", elem.element.getAttributeNode("id").?.attribute.value);
+    try testing.expectEqualStrings("a", elem.element.getAttributeNode("class").?.attribute.value);
+    try testing.expectEqual(@as(?*Node, null), elem.element.getAttributeNode("style"));
+    try testing.expectEqual(@as(usize, 2), elem.element.attributes.items.len);
+
+    id.attribute.value = "2";
+    try elem.element.setAttributeNode(&elem, @constCast(&id));
+    try testing.expectEqualStrings("2", elem.element.getAttributeNode("id").?.attribute.value);
+
+    id.deinit();
+    class.deinit();
     elem.deinit();
 }
 
 test "Element.removeAttribute" {
+    var elem = Node{ .element = @constCast(&Element.init(testing.allocator, "a")) };
+    const id = Node{ .attribute = @constCast(&Attr.init("id", "1", &elem)) };
 
+    try elem.element.setAttributeNode(&elem, @constCast(&id));
+    try elem.element.removeAttribute("id");
+
+    try testing.expectEqual(@as(?*Node, null), elem.element.getAttributeNode("id"));
+
+    id.deinit();
+    elem.deinit();
 }
 
 test "Element.appendChild sets parent and adds child" {
@@ -359,78 +372,30 @@ test "NamedNodeList.length" {
 test "Attr.init" {}
 
 
-test "Render simple element with attributes and text" {
+test "Node.render" {
     var arena = std.heap.ArenaAllocator.init(testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var elem = Element.init(allocator, "div");
-    var elemNode = Node{ .element = &elem };
-    const newAttrId = try testing.allocator.create(Attr);
-    newAttrId.* = Attr{ .name = "id", .value = "test", .parentElement = &elemNode };
-    var attrNode = Node{ .attribute = newAttrId };
-    try elem.setAttributeNode(&elemNode, &attrNode);
-    var text = Text{ .content = "Hello, World!" };
-    var text_node = Node{ .text = &text };
-    _ = try elem.appendChild(&elemNode, &text_node);
+    var elem = Node{ .element = @constCast(&Element.init(allocator, "p")) };
+    const attr = Node{ .attribute = @constCast(&Attr.init("id", "1", &elem)) };
+    try elem.element.setAttributeNode(&elem, @constCast(&attr));
+
+    var a = Node{ .element = @constCast(&Element.init(allocator, "a")) };
+    var text = Node{ .text = @constCast(&Text{ .content = "Hi" }) };
+
+    // var doc = Node{ .document = @constCast(&Document.init(allocator)) };
+    // _ = try doc.document.appendChild(&doc, &elem);
+
+    _ = try a.appendChild(&text);
+    _ = try elem.appendChild(&a);
 
     var buffer = std.ArrayList(u8).init(allocator);
     defer buffer.deinit();
     try elem.render(buffer.writer());
 
-    const expected = "<div id=\"test\">Hello, World!</div>";
-    try testing.expectEqualStrings(expected, buffer.items);
-    testing.allocator.destroy(newAttrId);
-}
+    try testing.expectEqualStrings("<p id=\"1\"><a>Hi</a></p>", buffer.items);
 
-test "Render nested elements" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    var parent = Element.init(allocator, "div");
-    var parentNode = Node{ .element = &parent };
-    const newAttrClass = try testing.allocator.create(Attr);
-    newAttrClass.* = Attr{ .name = "class", .value = "container", .parentElement = &parentNode };
-    var attrNodeClass = Node{ .attribute = newAttrClass };
-    try parent.setAttributeNode(&parentNode, &attrNodeClass);
-
-    var child = Element.init(allocator, "span");
-    var text = Text{ .content = "Nested" };
-    var text_node = Node{ .text = &text };
-    var child_node = Node{ .element = &child };
-
-    _ = try child.appendChild(&child_node, &text_node);
-    _ = try parent.appendChild(&parentNode, &child_node);
-
-    var buffer = std.ArrayList(u8).init(allocator);
-    defer buffer.deinit();
-    try parent.render(buffer.writer());
-
-    const expected = "<div class=\"container\"><span>Nested</span></div>";
-    try testing.expectEqualStrings(expected, buffer.items);
-    testing.allocator.destroy(newAttrClass);
-}
-
-test "Document renders children" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    var doc = Document.init(allocator);
-    var doc_node = Node{ .document = &doc };
-    var elem = Element.init(allocator, "p");
-    var text = Text{ .content = "Document content" };
-    var text_node = Node{ .text = &text };
-    var elem_node = Node{ .element = &elem };
-
-    _ = try elem.appendChild(&elem_node, &text_node);
-    _ = try doc.appendChild(&doc_node, &elem_node);
-
-    var buffer = std.ArrayList(u8).init(allocator);
-    defer buffer.deinit();
-    try doc.render(buffer.writer());
-
-    const expected = "<p>Document content</p>";
-    try testing.expectEqualStrings(expected, buffer.items);
+    elem.deinit();
+    attr.deinit();
 }
